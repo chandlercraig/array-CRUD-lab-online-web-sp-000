0dbc7f1bdb30986e27aa3f00bad73df7e2cdb1b7
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-24 15:22:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  boardgames = ["Monopoly", "Scrabble", "Catan", "Sorry"]
+  boardgames[3]
 end
 
 def retrieve_first_element_from_array(array)
