0c0e0f05d3b8491ad2bcd6e4de7e110945b5308c
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-24 15:21:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,9 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  boardgames = ["Monopoly", "Scrabble", "Catan", "Sorry"]
+  boardgames.shift
+  boardgames
 end
 
 def retrieve_element_from_index(array, index_number)
