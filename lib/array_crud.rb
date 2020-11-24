b4b060aa8c8ac0bcbca2837704c05527ebe633f6
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-24 15:24:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  boardgames = ["Monopoly", "Scrabble", "Catan", "Sorry"]
-  boardgames << "Chess"
-  boardgames
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
